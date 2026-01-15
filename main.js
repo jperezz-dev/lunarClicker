680b2cfb6f73d394eae7e4b3f22061b1e385b570
SpookyLuna <spookyluna666@gmail.com>
2026-01-15 22:00:45 +0100

    Commit inicial
    - Componentes menu y juego
    - Diseño del componente menu y router hasta juego

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'dist/lunar-clicker/browser/index.html'));
+}
+
+app.whenReady().then(createWindow);
---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { Menu } from './components/menu/menu';
+import { Juego } from './components/juego/juego';
+
+export const routes: Routes = [
+  { path: '', component: Menu },
+  { path: 'juego', component: Juego }
+];
