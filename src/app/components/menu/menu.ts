9b4bddb22f94a3f7a0f8de01c4fb002d7f570b00
SpookyLuna <spookyluna666@gmail.com>
2026-01-16 18:55:35 +0100

    Funcionalidad botones menú
    - Botón de pausar y reanudar música del menú
    - Botón de salir

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,30 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
 export class Menu implements OnInit, OnDestroy {
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
   // Definición de la música
   musica = new Audio('./assets/music/menu_musica.mp3');
+  musicaSonando = true;
 
   // Opciones del init (Loop, volumen y error)
   ngOnInit() {
     this.musica.loop = true;
     this.musica.volume = 0.5;
     this.reproducirMusica();
-
-    window.addEventListener(
-      'click',
-      () => {
-        this.reproducirMusica();
-      },
-      { once: true }
-    );
   }
 
   // La música se detiene al "destruir" el componente (Cambiar de ventana)
@@ -36,10 +33,32 @@ export class Menu implements OnInit, OnDestroy {
     this.musica
       .play()
       .then(() => {
+        this.musicaSonando = true;
         console.log('Musica sonando correctamente');
       })
       .catch((error) => {
         console.log('Esperando clic del usuario para sonar...');
       });
+  }
+
+  // Si la música está sonando se pausa, si está pausada se playea
+  pausarMusica() {
+    console.log("Estado antes:", this.musicaSonando);
+    if (this.musicaSonando == true) {
+      this.musica
+      .pause();
+      this.musicaSonando = false;
+    } else {
+      this.musica
+      .play()
+      .then(() => {
+        this.musicaSonando = true;
+        this.cdr.detectChanges();
+      });
+    }
+  }
+
+  cerrarJuego() {
+    window.close();
   }
 }
